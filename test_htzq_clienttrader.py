a4e09c1c258a29a5f7aeec11c1c81ea7ce6fba54
MG <mmmaaaggg@163.com>
2017-08-31 17:29:01 +0800

    support huatai zhegnquan

--- test_htzq_clienttrader.py
+++ test_htzq_clienttrader.py
@@ -7,8 +7,8 @@ import base64
 class TestEasytrader(unittest.TestCase):
 
     def test_login(self):
-        user_str = '050000006153'
-        password_str = '820901'  # bytes.decode(base64.decodebytes(b''))
+        user_str = '050000006***'
+        password_str = bytes.decode(base64.decodebytes(b''))
         # user = easytrader.use('ht_client')
         # user.prepare(user='100053072', password=password_str)
         user = easytrader.use('hb_client')
