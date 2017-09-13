de848d74a7e302af532f3f14c3fe8851e7fb589e
MG <mmmaaaggg@163.com>
2017-09-13 11:00:13 +0800

    fix tiny

--- test_gzzq_clienttrader.py
+++ test_gzzq_clienttrader.py
@@ -8,7 +8,7 @@ class TestEasytrader(unittest.TestCase):
 
     def test_login(self):
         user_str = '3300055265'
-        password_str = '541130'  # bytes.decode(base64.decodebytes(b'123123'))
+        password_str = '***'  # bytes.decode(base64.decodebytes(b'123123'))
         user = easytrader.use('gzzq_client')
         # user.prepare(user='100053072', password=password_str)
         # user = easytrader.use('hb_client')
